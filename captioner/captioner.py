6f523f984d057b8c7fa3c80fde77f4726026784d
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2024-11-03 22:43:20 +0300

    feat: add captioner model

---
+++ captioner/captioner.py
@@ -0,0 +1,25 @@
+import torch
+from transformers import pipeline
+
+captioner = None
+PROMPT = "The main subject of this picture is a"
+
+
+def init(device):
+    global captioner
+
+    print("Initializing captioner...")
+
+    captioner = pipeline(
+        "image-to-text",
+        model="Salesforce/blip-image-captioning-base",
+        prompt=PROMPT,
+        device=device,
+    )
+
+
+def derive_caption(image):
+    result = captioner(image, max_new_tokens=20)
+    raw_caption = result[0]["generated_text"]
+    caption = raw_caption.lower().replace(PROMPT.lower(), "").strip()
+    return caption
